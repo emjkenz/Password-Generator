e8c51066228a787e4baa09e5f239d5e51d5b8881
emjkenz <emily.baker1993@gmail.com>
2023-03-02 23:49:19 +1100

    Added checking for character length and character sets

--- SRC/script.js
+++ SRC/script.js
@@ -3,20 +3,29 @@ var generateBtn = document.querySelector("#generate");
 
 //Character sets
 var alphabet = "abcdefghijklmnopqrstuvwxyz"
-var specialch = "!@#$%^&*()_+[]{},.-?~"
+var specialch = "!@#$%^&*()_+[]{},.<>-?~"
 var numbers = "1234567890"
 
 var characters = "";
 
 function generatePassword(){
-  var passlength = prompt("Choose the length of your password.")
-  if (passlength<8||passlength>128){
-    alert ("At least 8 characters required.\nNo more than 128 characters.")
+  var passlength=0
+  while (passlength<8||passlength>128){
+    passlength = prompt("Choose the length of your password.")
+    if (passlength<8||passlength>128){
+      alert ("At least 8 characters required.\nNo more than 128 characters.")
+    }
   }
-  var lowercase = confirm("Do you want to include lowercase characters?")
-  var uppercase = confirm("Do you want to include uppercase characters?")
-  var numeric = confirm("Do you want to include numeric characters?")
-  var special = confirm("Do you want to include special characters?")
+  var lowercase,uppercase,numeric,special = false
+  while(!lowercase && !uppercase && !numeric && !special){
+    lowercase = confirm("Do you want to include lowercase characters?")
+    uppercase = confirm("Do you want to include uppercase characters?")
+    numeric = confirm("Do you want to include numeric characters?")
+    special = confirm("Do you want to include special characters?")
+    if (!lowercase && !uppercase && !numeric && !special) {
+      alert ("At least 1 character set is needed.")
+  }
+}
   if (lowercase) characters+=alphabet
   if (uppercase) characters+=alphabet.toUpperCase()
   if (numeric) characters+=numbers
